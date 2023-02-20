7f3019841b2f803d1619b870e2e9f85ab941d29a
Twerkky <118483218+Twerkky@users.noreply.github.com>
2023-02-20 17:32:43 +0200

    Update code.js

--- code.js
+++ code.js
@@ -17,6 +17,25 @@ function getRate() {
       }))
     });
   }
- 
+//  function getRate() {
+//   let CodeInput = document.getElementById("Code");
+//   let dateInput = document.getElementById("date");
+
+//   let Code = CodeInput.value;
+//   let date = dateInput.value;
+
+//   let url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=" + Code + "&date=" + date;
+
+//   fetch(url)
+//     .then(response => response.text())
+//     .then(data => {
+//       let parser = new DOMParser();
+//       let xml = parser.parseFromString(data, "application/xml");
+//       let rate = xml.querySelector("rate").textContent;
+//       let result = document.getElementById("exRate");
+//       result.innerHTML = "Курс: " + rate + "₴ = 1 " + Code;
+//     })
+//     .catch(error => console.log(error));
+// }
     
   
